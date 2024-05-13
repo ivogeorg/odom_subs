ae956d5497948dc126f283985d2da0c423697c83
Ivo Georgiev <ivogeorg@gmail.com>
2024-05-13 00:42:57 +0000

    from position to velocity

--- src/odom_listener.cpp
+++ src/odom_listener.cpp
@@ -2,19 +2,21 @@
 #include "ros/node_handle.h"
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
-#include "../../test_library/include/test_library/test_library.h"
+#include "../../velocity_library/include/velocity_library/vlib.hpp"
 
 void odomCallback(const nav_msgs::OdometryConstPtr &msg) {
     float x = msg->pose.pose.position.x;
     float y = msg->pose.pose.position.y;
+    float linx = msg->twist.twist.linear.x;
+    float angz = msg->twist.twist.angular.z;
     ros::Duration(1).sleep();
-    display_pos(x, y);
+    display_velocity(linx, angz);
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "odom_listener");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("odom", 1000, odomCallback);
+    ros::Subscriber sub = n.subscribe("odom", 1, odomCallback);
     ros::spin();
     
     return 0;
